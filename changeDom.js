452cb811093fb4e9bd6f5cfe436f0e7f4240bf86
pouria.moosavi <pouria.moosvi@gmail.com>
2020-11-16 21:32:23 +0330

    add some icones and make more beutiful!

--- changeDom.js
+++ changeDom.js
@@ -13,17 +13,38 @@ function checkIfCardInner(cb) {
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   try {
-    var response = JSON.parse(request.response)
-    var projects = response.result.projects;
-    var users = response.result.users;
+    let response = JSON.parse(request.response)
+    let projects = response.result.projects;
+    let users = response.result.users;
 
     checkIfCardInner(function cardInnerExists() {
-      var projectDivs = document.getElementsByClassName("info-card-inner");
-      for (var i = 0; i < projects.length; i++) {
-        var ownerID = projects[i].owner_id;
-        var ownerDisplayName = users[ownerID].display_name;
-        var ownerUsername = users[ownerID].username;
-        projectDivs[i].innerHTML += "<hr><p>" + ownerDisplayName + " (" + ownerUsername + ")" + "</p>";
+      try {
+        let projectDivs = document.getElementsByClassName("search-result-item");
+        for (let i = 0; i < projects.length; i++) {
+          let ownerID = projects[i].owner_id;
+          let ownerDisplayName = users[ownerID].display_name || "";
+          let ownerUsername = users[ownerID].username || "";
+          let loc = users[ownerID].location.country.name || "";
+          let { complete = 0 } = users[ownerID].employer_reputation.entire_history;
+          let regDate = new Date(users[ownerID].registration_date * 1000)
+          let regYear = regDate.getFullYear() || 0;
+          let { payment_verified: pav,
+            email_verified: em,
+            deposit_made: dm,
+            profile_complete: pc,
+            phone_verified: phv,
+            identity_verified: iv,
+            facebook_connected: fc,
+            freelancer_verified_user: fvu
+          } = users[ownerID].status;
+          projectDivs[i].innerHTML +=
+            `<hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
+            <span>pay ver: ${pav ? "✔" : "✘"} | em ver: ${em ? "✔" : "✘"} | dep made: ${dm ? "✔" : "✘"} | prof com: ${pc ? "✔" : "✘"} |
+             phn ver: ${phv ? "✔" : "✘"} | id ver: ${iv ? "✔" : "✘"} | fcb con: ${fc ? "✔" : "✘"} | ver user: ${fvu ? "✔" : "✘"}</span>
+          `;
+        }
+      } catch (err) {
+        console.log(err)
       }
     })
   } catch (err) {

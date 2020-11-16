f1d4a86b67dc2bcf5d994af01e655e08e8580826
pouria.moosavi <pouria.moosvi@gmail.com>
2020-11-16 21:32:28 +0330

    add some icones and make more beutiful!

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
+// browser.runtime.onMessage.addListener(function capt
 
+// )
 function listener(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   let decoder = new TextDecoder("utf-8");
@@ -10,7 +12,7 @@ function listener(details) {
     response += str;
   }
   filter.onstop = () => {
-    sendProjectsToChangeDom(response);
+    sendResponseToChangeDom(response);
     filter.disconnect();
   };
 
@@ -29,7 +31,7 @@ browser.webRequest.onBeforeRequest.addListener(
 // //https://www.freelancer.com/api/projects/0.1/projects/active/?compact=true&enterprise_metadata_field_details=true&forceShowLocationDetails=false&full_description=true&job_details=true&jobs%5B%5D=9&jobs%5B%5D=13&jobs%5B%5D=31&jobs%5B%5D=500&jobs%5B%5D=759&jobs%5B%5D=68&jobs%5B%5D=343&jobs%5B%5D=598&jobs%5B%5D=287&jobs%5B%5D=1254&jobs%5B%5D=619&jobs%5B%5D=1082&jobs%5B%5D=270&jobs%5B%5D=602&jobs%5B%5D=43&jobs%5B%5D=440&jobs%5B%5D=1347&keywords=&languages%5B%5D=en&limit=10&offset=0&project_types%5B%5D=fixed&query=&sort_field=submitdate&upgrade_details=true&user_details=true&user_employer_reputation=true&user_status=true
 
 
-function sendProjectsToChangeDom(response) {
+function sendResponseToChangeDom(response) {
   setTimeout(() => {
     let querying = browser.tabs.query({
       active: true,
@@ -50,38 +52,21 @@ function sendProjectsToChangeDom(response) {
 }
 
 // browser.contextMenus.create({
-//   id: "changeDom",
-//   title: "Eat this page"
+//   id: "toggleRevealancer",
+//   title: "enable/disable Revealancer",
 // });
 
-// function messageTab(tabs) {
-//   browser.tabs.sendMessage(tabs[0].id, {
-//     projects: 100
-//   });
-// }
-
-// function onExecuted(result) {
-//   let querying = browser.tabs.query({
-//     active: true,
-//     currentWindow: true
-//   });
-//   querying.then(messageTab);
-// }
-
-// function sendProjectsToChangeDom(info, tab) {
-//   //if (info.menuItemId == "changeDom") {
-//   // onExecuted()
-//   let executing = browser.tabs.executeScript({
-//     file: "changeDom.js"
-//   });
-//   executing.then(onExecuted);
-//   //}
-// };
-
-// function docReady(fn) {
-//   if (document.readyState === "complete" || document.readyState === "interactive") {
-//     setTimeout(fn, 1);
-//   } else {
-//     document.addEventListener("DOMContentLoaded", fn);
+// browser.contextMenus.onClicked.addListener((info, tab) => {
+//   if (info.menuItemId == "toggleRevealancer") {
+    // let querying = browser.tabs.query({
+    //   active: true,
+    //   currentWindow: true
+    // });
+    // console.log(1)
+    // querying.then((tabs) => {
+    //   browser.tabs.sendMessage(tabs[0].id, {
+    //     toggleRevealancer: true,
+    //   });
+    // });
 //   }
-// }
+// })
--- changeDom.js
+++ changeDom.js
@@ -1,8 +1,39 @@
 
+function createRevealancerInfo({ projects, users }) {
+  checkIfCardInner(function cardInnerExists() {
+    try {
+      let projectDivs = document.getElementsByClassName("search-result-item");
+      for (let i = 0; i < projects.length; i++) {
+        let ownerID = projects[i].owner_id;
+        let ownerDisplayName = users[ownerID].display_name || "";
+        let ownerUsername = users[ownerID].username || "";
+        let loc = users[ownerID].location.country.name || "";
+        let { complete = 0 } = users[ownerID].employer_reputation.entire_history;
+        let regDate = new Date(users[ownerID].registration_date * 1000)
+        let regYear = regDate.getFullYear() || 0;
+        let { payment_verified: pav,
+          email_verified: em,
+          deposit_made: dm,
+          profile_complete: pc,
+          phone_verified: phv,
+          identity_verified: iv,
+          facebook_connected: fc,
+          freelancer_verified_user: fvu
+        } = users[ownerID].status;
+        projectDivs[i].innerHTML +=
+          `<div class="revealancer-info"><hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
+          <span>pay ver: ${pav ? "✔" : "✘"} | em ver: ${em ? "✔" : "✘"} | dep made: ${dm ? "✔" : "✘"} | prof com: ${pc ? "✔" : "✘"} |
+           phn ver: ${phv ? "✔" : "✘"} | id ver: ${iv ? "✔" : "✘"} | fcb con: ${fc ? "✔" : "✘"} | ver user: ${fvu ? "✔" : "✘"}</span></div>
+        `;
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  })
+}
+
 function checkIfCardInner(cb) {
   setTimeout(() => {
-    console.log(100)
-
     if (document.getElementsByClassName("info-card-inner")[0]) {
       return cb()
     } else {
@@ -11,42 +42,27 @@ function checkIfCardInner(cb) {
   }, 10);
 }
 
+function toggleRevealancerInfo() {
+  let revealancerInfoDivs = document.getElementsByClassName("revealancer-info");
+  let currentStatus = revealancerInfoDivs[0] && revealancerInfoDivs[0].getAttribute("display")
+
+  let newStatus = currentStatus === "block" ? "none" : "block";
+  for (let i = 0; i < revealancerInfoDivs.length; i++) {
+    revealancerInfoDivs[i].setAttribute("display", newStatus)
+  }
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   try {
-    let response = JSON.parse(request.response)
-    let projects = response.result.projects;
-    let users = response.result.users;
-
-    checkIfCardInner(function cardInnerExists() {
-      try {
-        let projectDivs = document.getElementsByClassName("search-result-item");
-        for (let i = 0; i < projects.length; i++) {
-          let ownerID = projects[i].owner_id;
-          let ownerDisplayName = users[ownerID].display_name || "";
-          let ownerUsername = users[ownerID].username || "";
-          let loc = users[ownerID].location.country.name || "";
-          let { complete = 0 } = users[ownerID].employer_reputation.entire_history;
-          let regDate = new Date(users[ownerID].registration_date * 1000)
-          let regYear = regDate.getFullYear() || 0;
-          let { payment_verified: pav,
-            email_verified: em,
-            deposit_made: dm,
-            profile_complete: pc,
-            phone_verified: phv,
-            identity_verified: iv,
-            facebook_connected: fc,
-            freelancer_verified_user: fvu
-          } = users[ownerID].status;
-          projectDivs[i].innerHTML +=
-            `<hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
-            <span>pay ver: ${pav ? "✔" : "✘"} | em ver: ${em ? "✔" : "✘"} | dep made: ${dm ? "✔" : "✘"} | prof com: ${pc ? "✔" : "✘"} |
-             phn ver: ${phv ? "✔" : "✘"} | id ver: ${iv ? "✔" : "✘"} | fcb con: ${fc ? "✔" : "✘"} | ver user: ${fvu ? "✔" : "✘"}</span>
-          `;
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    })
+    if (request.response) {
+      let response = JSON.parse(request.response)
+      let projects = response.result.projects;
+      let users = response.result.users;
+      createRevealancerInfo({ projects, users })
+    } else if (request.toggleRevealancer) {
+      console.log(200)
+      toggleRevealancerInfo()
+    }
   } catch (err) {
     console.log(err)
   }

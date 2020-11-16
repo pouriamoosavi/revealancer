41fbe35222120d08df7e68e4d965d2550a2711ca
pouria.moosavi <pouria.moosvi@gmail.com>
2020-11-17 01:51:44 +0330

    remove listener comments. add contextMenu for enable and disable

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
+var senderId;
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  senderId = sender.tab.id
+  console.log(1000)
   if (request.captureInfo) {
     browser.webRequest.onBeforeRequest.addListener(
       listener,
@@ -10,12 +13,7 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     );
 
     function sendInfo(response) {
-      // setTimeout(() => {
       sendResponse(response)
-      // browser.tabs.sendMessage(sender.tab.id, {
-      //   response
-      // });
-      // }, 1);
     }
 
     function listener(details) {
@@ -36,25 +34,21 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       return {};
     }
   }
-
-  browser.contextMenus.create({
-    id: "toggleRevealancer",
-    title: "enable/disable Revealancer",
-  });
-
-  browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId == "toggleRevealancer") {
-      browser.tabs.executeScript(sender.tab.id, {
-        code: "toggleRevealancerInfo()"
-      });
-      // browser.tabs.sendMessage(sender.tab.id, {
-      //   toggleRevealancer: true,
-      // });
-    }
-  })
   return true;
 })
 
+browser.contextMenus.create({
+  id: "toggleRevealancer",
+  title: "enable/disable Revealancer",
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId == "toggleRevealancer") {
+    browser.tabs.executeScript(senderId, {
+      code: "toggleRevealancerInfo()"
+    });
+  }
+})
 // //https://www.freelancer.com/api/projects/0.1/projects/active/?compact=true&enterprise_metadata_field_details=true&forceShowLocationDetails=false&full_description=true&job_details=true&jobs%5B%5D=9&jobs%5B%5D=13&jobs%5B%5D=31&jobs%5B%5D=500&jobs%5B%5D=759&jobs%5B%5D=68&jobs%5B%5D=343&jobs%5B%5D=598&jobs%5B%5D=287&jobs%5B%5D=1254&jobs%5B%5D=619&jobs%5B%5D=1082&jobs%5B%5D=270&jobs%5B%5D=602&jobs%5B%5D=43&jobs%5B%5D=440&jobs%5B%5D=1347&keywords=&languages%5B%5D=en&limit=10&offset=0&project_types%5B%5D=fixed&query=&sort_field=submitdate&upgrade_details=true&user_details=true&user_employer_reputation=true&user_status=true
 
 
--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function createRevealancerInfo({ projects, users }) {
           freelancer_verified_user: fvu
         } = users[ownerID].status;
         projectDivs[i].innerHTML +=
-          `<div class="revealancer-info"><hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
+          `<div class="revealancer-info" style="display:block">
+          <hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
           <span>pay ver: ${pav ? "✔" : "✘"} | em ver: ${em ? "✔" : "✘"} | dep made: ${dm ? "✔" : "✘"} | prof com: ${pc ? "✔" : "✘"} |
            phn ver: ${phv ? "✔" : "✘"} | id ver: ${iv ? "✔" : "✘"} | fcb con: ${fc ? "✔" : "✘"} | ver user: ${fvu ? "✔" : "✘"}</span></div>
         `;
@@ -52,35 +53,9 @@ function checkIfCardInner(cb) {
 
 function toggleRevealancerInfo() {
   let revealancerInfoDivs = document.getElementsByClassName("revealancer-info");
-  console.log(revealancerInfoDivs)
-  let currentStatus = revealancerInfoDivs[0] && revealancerInfoDivs[0].getAttribute("display")
-  console.log(currentStatus)
-
+  let currentStatus = revealancerInfoDivs[0] && revealancerInfoDivs[0].style.display
   let newStatus = currentStatus === "block" ? "none" : "block";
-  console.log(newStatus)
   for (let i = 0; i < revealancerInfoDivs.length; i++) {
-    revealancerInfoDivs[i].setAttribute("display", newStatus)
+    revealancerInfoDivs[i].style.display = newStatus;
   }
 }
-
-// function listener(request, sender, sendResponse) {
-//   try {
-//     if (request.response) {
-//       console.log("request.response")
-//       let response = JSON.parse(request.response)
-//       let projects = response.result.projects;
-//       let users = response.result.users;
-//       createRevealancerInfo({ projects, users })
-//     } else if (request.toggleRevealancer) {
-//       console.log(200)
-//       toggleRevealancerInfo()
-//     }
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
-// browser.runtime.onMessage.hasListener((has) => {
-//   console.log(has)
-// })
-// browser.runtime.onMessage.removeListener(listener);
-// browser.runtime.onMessage.addListener(listener);

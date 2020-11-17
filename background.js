a753550b8ed972b4135e24ad1db76d02896d8bea
pouria.moosavi <pouria.moosvi@gmail.com>
2020-11-17 20:34:10 +0330

    Use html safe asignment (DOMParser). Add title.

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ browser.webRequest.onBeforeRequest.addListener(
 function listenerForFreelancerProjects(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   let decoder = new TextDecoder("utf-8");
-  var response = "";
+  let response = "";
 
   filter.ondata = (event) => {
     filter.write(event.data);
@@ -60,8 +60,3 @@ browser.menus.create({
 });
 
 // //https://www.freelancer.com/api/projects/0.1/projects/active/?compact=true&enterprise_metadata_field_details=true&forceShowLocationDetails=false&full_description=true&job_details=true&jobs%5B%5D=9&jobs%5B%5D=13&jobs%5B%5D=31&jobs%5B%5D=500&jobs%5B%5D=759&jobs%5B%5D=68&jobs%5B%5D=343&jobs%5B%5D=598&jobs%5B%5D=287&jobs%5B%5D=1254&jobs%5B%5D=619&jobs%5B%5D=1082&jobs%5B%5D=270&jobs%5B%5D=602&jobs%5B%5D=43&jobs%5B%5D=440&jobs%5B%5D=1347&keywords=&languages%5B%5D=en&limit=10&offset=0&project_types%5B%5D=fixed&query=&sort_field=submitdate&upgrade_details=true&user_details=true&user_employer_reputation=true&user_status=true
-
-
-
-
-
--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function createRevealancerInfo(response) {
     try {
       let projectDivs = document.getElementsByClassName("search-result-item");
       for (let i = 0; i < projects.length; i++) {
-        let ownerID = projects[i].owner_id;
-        let ownerDisplayName = users[ownerID].display_name || "";
-        let ownerUsername = users[ownerID].username || "";
-        let loc = users[ownerID].location.country.name || "";
-        let { complete = 0 } = users[ownerID].employer_reputation.entire_history;
-        let regDate = new Date(users[ownerID].registration_date * 1000)
-        let regYear = regDate.getFullYear() || 0;
-        let { payment_verified: pav,
+        const ownerID = projects[i].owner_id;
+        const ownerDisplayName = users[ownerID].display_name || "";
+        const ownerUsername = users[ownerID].username || "";
+        const loc = users[ownerID].location.country.name || "";
+        const { complete = 0 } = users[ownerID].employer_reputation.entire_history;
+        const regDate = new Date(users[ownerID].registration_date * 1000)
+        const regYear = regDate.getFullYear() || 0;
+        const { payment_verified: pav,
           email_verified: em,
           deposit_made: dm,
           profile_complete: pc,
@@ -22,12 +22,23 @@ function createRevealancerInfo(response) {
           facebook_connected: fc,
           freelancer_verified_user: fvu
         } = users[ownerID].status;
-        projectDivs[i].innerHTML +=
-          `<div class="revealancer-info" style="display:block">
-          <hr style="margin: 10px 0"><span> ${ownerDisplayName} (${ownerUsername}) | ${loc} | complete: ${complete} | ${regYear} </span><br>
-          <span>pay ver: ${pav ? "✔" : "✘"} | em ver: ${em ? "✔" : "✘"} | dep made: ${dm ? "✔" : "✘"} | prof com: ${pc ? "✔" : "✘"} |
-           phn ver: ${phv ? "✔" : "✘"} | id ver: ${iv ? "✔" : "✘"} | fcb con: ${fc ? "✔" : "✘"} | ver user: ${fvu ? "✔" : "✘"}</span></div>
-        `;
+        const infoHtml = `<div class="revealancer-info" style="display:block">
+          <hr style="margin: 10px 0"><div> ${ownerDisplayName} (${ownerUsername}) 
+          | ${loc} | complete: ${complete} | ${regYear} </div>
+          <div><span title="Payment Verified">pay ver: ${pav ? "✔" : "✘"}</span> 
+          | <span title="Email Verified">em ver: ${em ? "✔" : "✘"}</span> 
+          | <span title="Deposit Made">dep made: ${dm ? "✔" : "✘"}</span> 
+          | <span title="Profile Complete">prof com: ${pc ? "✔" : "✘"}</span> 
+          | <span title="Phone Verified">phn ver: ${phv ? "✔" : "✘"}</span> 
+          | <span title="Identity Verified">id ver: ${iv ? "✔" : "✘"}</span>
+          | <span title="Facebook Connected">fcb con: ${fc ? "✔" : "✘"}</span> 
+          | <span title="Freelancer Verified User">ver user: ${fvu ? "✔" : "✘"}</span>
+          </div></div>
+        `
+        const parser = new DOMParser();
+        const parsed = parser.parseFromString(infoHtml, "text/html");
+        const tags = parsed.getElementsByClassName("revealancer-info");
+        projectDivs[i].appendChild(tags[0]);
       }
     } catch (err) {
       console.log(err)
@@ -47,8 +58,8 @@ function checkIfCardInner(cb) {
 
 function toggleRevealancerInfo() {
   let revealancerInfoDivs = document.getElementsByClassName("revealancer-info");
-  let currentStatus = revealancerInfoDivs[0] && revealancerInfoDivs[0].style.display
-  let newStatus = currentStatus === "block" ? "none" : "block";
+  const currentStatus = revealancerInfoDivs[0] && revealancerInfoDivs[0].style.display
+  const newStatus = currentStatus === "block" ? "none" : "block";
   for (let i = 0; i < revealancerInfoDivs.length; i++) {
     revealancerInfoDivs[i].style.display = newStatus;
   }
